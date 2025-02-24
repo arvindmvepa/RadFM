9e9b21846f8462d366201d1f46081391536004dd
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-24 12:18:44 -0800

    bug fix

--- src/train.py
+++ src/train.py
@@ -85,7 +85,8 @@ class DataCollator(object):
         vision_xs = torch.nn.utils.rnn.pad_sequence(
             vision_xs, batch_first=True, padding_value=0
         )
-        print(vision_xs.shape,vision_xs.dtype, np.mean(vision_xs))
+        print(vision_xs.shape,vision_xs.dtype)
+        print(torch.mean(vision_xs),torch.std(vision_xs))
         return dict(
             lang_x=lang_xs,
             vision_x=vision_xs,
