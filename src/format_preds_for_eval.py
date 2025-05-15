ec5875dc38ab94676940636d242788e9b77d8d4a
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-05-15 04:16:09 -0700

    clean preds for radfm

--- src/format_preds_for_eval.py
+++ src/format_preds_for_eval.py
@@ -39,12 +39,13 @@ def clean_prediction(predicted_text: str) -> str:
 
 
 if __name__ == "__main__":
-    pred_gt_files = [("/local2/amvepa91/RadFM/src/gli_run/brats_gli_3d_vqa_subjTrue_test_v3.json.test.csv",
-                   "/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v3.json"),
-                  ("/local2/amvepa91/RadFM/src/met_run/brats_met_3d_vqa_subjTrue_test_v1.json.test.csv",
-                   "/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_v1.json"),
-                  ("/local2/amvepa91/RadFM/src/goat_run/brats_goat_3d_vqa_subjTrue_test_v1.json.test.csv",
-                   "/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_test_v1.json")]
+    pred_gt_files = [("/local2/amvepa91/RadFM/src/met_new_dataset_v3_run/brats_met_3d_vqa_subjTrue_test_updated_v3_seed0_multitask_fixed.json.test.csv",
+                   "/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_updated_v2_seed0_multitask_fixed.json"),
+                     #("/local2/amvepa91/RadFM/src/met_run/brats_met_3d_vqa_subjTrue_test_v1.json.test.csv",
+                   #"/local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_v1.json"),
+                  #("/local2/amvepa91/RadFM/src/goat_run/brats_goat_3d_vqa_subjTrue_test_v1.json.test.csv",
+                   #"/local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_test_v1.json")
+                     ]
     for pred_file, gt_file in pred_gt_files:
         print("Cleaning predictions in", pred_file)
         df = pd.read_csv(pred_file)
