b36034f1bf7488a3f5bb58ae5fa7df6162840b78
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-07-27 20:47:28 +0100

    bug fix

--- src/eval_new_dataset.sh
+++ src/eval_new_dataset.sh
@@ -3,4 +3,4 @@
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python test.py \
 --model_path /local2/amvepa91/RadFM/src/gli_new_dataset_v10_run/checkpoint-93312/pytorch_model.bin \
---test_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v10_seed0.json --output_dir ./gli_new_dataset_v10_run
+--test_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_updated_v10_seed0_multitask_fixed.json --output_dir ./gli_new_dataset_v10_run
