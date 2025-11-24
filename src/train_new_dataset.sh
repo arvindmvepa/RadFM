b05824560ac65711896ff1019d13a9dfae1d8739
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-24 00:27:20 -0800

    radfm training

--- src/train_new_dataset.sh
+++ src/train_new_dataset.sh
@@ -2,6 +2,6 @@
 
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python train.py \
---train_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v10_seed0_multitask_fixed.json \
---val_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v10_seed0_multitask_fixed.json \
---output_dir ./gli_new_dataset_v10_run
+--train_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json \
+--val_data_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json \
+--output_dir ./gli_new_dataset_v11_run
