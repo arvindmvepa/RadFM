8c861b5f4e737b842fc84e22d2da4b74efbf4df6
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-28 16:04:11 -0800

    add goat script for v11

---
+++ src/train_goat_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python train.py \
+--train_data_path /local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json \
+--val_data_path /local2/amvepa91/MedTrinity-25M/brats_goat_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json \
+--output_dir ./goat_new_dataset_v11_run
